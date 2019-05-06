cd18bd159bb735b2c38cbaac85375845261fb53f
RafaelaSilveira <rafaelacss21@gmail.com>
2019-05-06 16:17:31 -0400

    finish expense 1:09h

--- js/app.js
+++ js/app.js
@@ -39,7 +39,87 @@ class UI {
     }
   }
   showBalance() {
-    console.log("hey im working");
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+    if (total < 0) {
+      this.balance.classList.remove("showGreen", "showBlack");
+      this.balance.classList.add("showRed");
+    } else if (total > 0) {
+      this.balance.classList.remove("showRed", "showBlack");
+      this.balance.classList.add("showGreen");
+    } else if (total === 0) {
+      this.balance.classList.remove("showRed", "showGreen");
+      this.balance.classList.add("showBlack");
+    }
+  }
+  //submit expense form
+  submitExpenseForm() {
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    if (expenseValue === "" || amountValue === "") {
+      this.expenseFeedback.classList.add("showItem");
+      this.expenseFeedback.innerHTML = `<p>Values cannot be empty</p>`;
+      const self = this;
+      setTimeout(function() {
+        self.expenseFeedback.classList.remove("showItem");
+      }, 4000);
+    } else if (amountValue < 0) {
+      this.expenseFeedback.classList.add("showItem");
+      this.expenseFeedback.innerHTML = `<p>Amount cannot be less than zero</p>`;
+      const self = this;
+      setTimeout(function() {
+        self.expenseFeedback.classList.remove("showItem");
+      }, 4000);
+    } else {
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = "";
+      this.amountInput.value = "";
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount: amount
+      };
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      this.showBalance();
+    }
+  }
+  //add expense
+  addExpense(expense) {
+    const div = document.createElement("div");
+    div.classList.add("expense");
+    div.innerHTML = `
+      <div class="expense-item d-flex justify-content-between align-items-baseline">
+         <h6 class="expense-title mb-0 text-uppercase list-item">${
+           expense.title
+         }</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+         <div class="expense-icons list-item">
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+       `;
+    this.expenseList.appendChild(div);
+  }
+
+  totalExpense() {
+    let total = 0;
+    if (this.itemList.length > 0) {
+      total = this.itemList.reduce(function(acc, curr) {
+        console.log(`total is ${acc} and the current value is ${curr.amount}`);
+        acc += curr.amount;
+        return acc;
+      }, 0);
+    }
+    this.expenseAmount.textContent = total;
+    return total;
   }
 }
 
@@ -60,6 +140,7 @@ function eventListeners() {
   //expense form submit
   expenseForm.addEventListener("submit", function(event) {
     event.preventDefault();
+    ui.submitExpenseForm();
   });
 
   //expense click
